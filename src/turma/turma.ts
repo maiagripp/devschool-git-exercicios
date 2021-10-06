02e5ae6388de39a38020da52d48807c25f7049ba
Claudia Maia Gripp <44818833+maiagripp@users.noreply.github.com>
2021-10-06 20:52:11 -0300

    feat: adicionando claudia maia como constante

--- src/turma/turma.ts
+++ src/turma/turma.ts
@@ -1,5 +1,6 @@
 import { ALAN_JHONNES } from "../alunos/alan-jhonnes";
+import { CLAUDIA_MAIA } from "../alunos/claudia-maia";
 
 export const turma: string[] = [
-    ALAN_JHONNES,
+    ALAN_JHONNES, CLAUDIA_MAIA
 ]
